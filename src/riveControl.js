69cbdfe36c5d84fcba7a6744aeb7bd0bb9c3b84f
NbtKmy <nobuta77to99@hotmail.com>
2023-03-17 15:50:07 +0100

    some changes

--- src/riveControl.js
+++ src/riveControl.js
@@ -1,3 +1,9 @@
+function hideVid(elmId) {
+  document.getElementById(elmId).style.visibility = "hidden";
+  location.reload();
+}
+
+let buttonCanvasExample = document.getElementById("canvas");
 const libraryCanvas = new rive.Rive({
     src: "https://nbtkmy.github.io/jahresbericht/assets/rive/Rive_TestmitLadina.riv",
     canvas: document.getElementById("canvas"),
@@ -6,21 +12,22 @@ const libraryCanvas = new rive.Rive({
     stateMachines: ["Ladina_Text_Blase"],
     onLoad: () => {
       libraryCanvas.resizeDrawingSurfaceToCanvas();
-      const inputs = libraryCanvas.stateMachineInputs("Ladina_Text_Blase");
-      /*
-      const xInput = inputs.find((input) => {
-        return input.name === "xAxis";
+    },
+    onStateChange: (riveEvent) => {
+      const newStates = riveEvent.data;
+      //console.log(newStates);
+      newStates.forEach((state) => {
+        //name of the state transitioned to on click and the URL you want
+        if (state.indexOf("Ladina_LinkOut") > -1) {
+          const vid1 = document.getElementById("vid1");
+          vid1.style.visibility = "visible";
+          
+          //Add an else if for all states that should have the 'pointer' cursor
+        } else if (state.indexOf("Ladina_TextBlase_Hover") > -1) {
+          buttonCanvasExample.style.cursor = "pointer";
+        } else {
+          buttonCanvasExample.style.cursor = "default";
+        }
       });
-  
-      const yInput = inputs.find((input) => {
-        return input.name === "yAxis";
-      });
-      const maxWidth = window.innerWidth;
-      const maxHeight = window.innerHeight;
-      window.addEventListener("mousemove", function (e) {
-        xInput.value = (e.x / maxWidth) * 100;
-        yInput.value = 100 - (e.y / maxHeight) * 100;
-      });
-      */
     }
   });
