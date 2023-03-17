563e4e93cdc2120904f8ae46c3a60da870e4fd9f
NbtKmy <nobuta77to99@hotmail.com>
2023-03-17 18:28:36 +0100

    some changes

--- src/riveControl.js
+++ src/riveControl.js
@@ -1,7 +1,3 @@
-function hideVid(elmId) {
-  document.getElementById(elmId).style.visibility = "hidden";
-  location.reload();
-}
 
 let buttonCanvasExample = document.getElementById("canvas");
 const libraryCanvas = new rive.Rive({
@@ -15,12 +11,15 @@ const libraryCanvas = new rive.Rive({
     },
     onStateChange: (riveEvent) => {
       const newStates = riveEvent.data;
-      //console.log(newStates);
+      console.log(newStates);
       newStates.forEach((state) => {
-        //name of the state transitioned to on click and the URL you want
+
         if (state.indexOf("Ladina_LinkOut") > -1) {
-          const vid1 = document.getElementById("vid1");
-          vid1.style.visibility = "visible";
+          const elemId = "popupContent";
+          const popupContent = document.getElementById(elemId);
+          popupContent.style.visibility = "visible";
+          popupContent.innerHTML += "<button type='button' class='kreuz' onclick='hideVid(" + elemId + ");'>X</button> \
+          <video src='./assets/vid/testVid.mov' controls></video>"
           
           //Add an else if for all states that should have the 'pointer' cursor
         } else if (state.indexOf("Ladina_TextBlase_Hover") > -1) {
@@ -31,3 +30,13 @@ const libraryCanvas = new rive.Rive({
       });
     }
   });
+
+
+  function hideVid(elm) {
+
+    const popupContent = document.getElementById(elm.id);
+    //popupContent.innerHTML = ""; // Dies ist nicht notwendig, solange die ganze Seite reloadet wird
+    popupContent.style.visibility = "hidden";
+    // Am besten sollte der Rive-State geändert werden...
+    location.reload(false);
+  }
