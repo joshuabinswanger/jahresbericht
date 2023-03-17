67a744c1b99537166685b8f3ad9fef0f5616db2d
NbtKmy <nobuta77to99@hotmail.com>
2023-03-17 11:03:30 +0100

    first commits from ...

---
+++ src/riveControl.js
@@ -0,0 +1,26 @@
+const libraryCanvas = new rive.Rive({
+    src: "./assets/rive/Rive_TestmitLadina.riv",
+    canvas: document.getElementById("canvas"),
+    autoplay: true,
+    artboard: "TestmitLadina",
+    stateMachines: ["Ladina_Text_Blase"],
+    onLoad: () => {
+      libraryCanvas.resizeDrawingSurfaceToCanvas();
+      const inputs = libraryCanvas.stateMachineInputs("Ladina_Text_Blase");
+      /*
+      const xInput = inputs.find((input) => {
+        return input.name === "xAxis";
+      });
+  
+      const yInput = inputs.find((input) => {
+        return input.name === "yAxis";
+      });
+      const maxWidth = window.innerWidth;
+      const maxHeight = window.innerHeight;
+      window.addEventListener("mousemove", function (e) {
+        xInput.value = (e.x / maxWidth) * 100;
+        yInput.value = 100 - (e.y / maxHeight) * 100;
+      });
+      */
+    }
+  });
