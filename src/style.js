098bd84adadbe779ce9ed6604f66855fa75a1c3f
NbtKmy <nobuta77to99@hotmail.com>
2023-03-30 10:30:58 +0200

    css/ canvas width changed

---
+++ src/style.js
@@ -0,0 +1,10 @@
+const width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+console.log(width);
+let canvas = document.getElementById("canvas");
+const canvas_width = width * 0.8;
+const adjust = (width - canvas_width) * 0.48;
+
+canvas.style.width = String(width * 0.8) + "px";
+canvas.style.height = String(width * 1.3) + "px";
+canvas.style.left = String(adjust) + "px";
+
