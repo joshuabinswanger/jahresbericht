a9b4d80735a9447cbfc935d3a11794db7a6d9a89
dlsubzh <105430898+dlsubzh@users.noreply.github.com>
2023-04-08 19:58:07 +0200

    Update riveControl.js

--- src/riveControl.js
+++ src/riveControl.js
@@ -19,7 +19,7 @@ const libraryCanvas = new rive.Rive({
           //const elemId = "popupContent";
           //const popupContent = document.getElementById(elemId);
           //popupContent.style.visibility = "visible";
-          /popupContent.innerHTML += "<span class='kreuz' onclick='hideVid(" + elemId + ");'>X</span> \
+          //popupContent.innerHTML += "<span class='kreuz' onclick='hideVid(" + elemId + ");'>X</span> \
           //<video src="https://player.vimeo.com/video/808776367?h=845b2ca585" controls></video>"
           
           //Kontrollieren: Mouse-Pointer-Formen 
